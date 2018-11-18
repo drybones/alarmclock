7e64b3c35342b90768e7375baf793c88915b6a43
Unknown <david.bick@red-gate.com>
2018-11-18 10:52:55 +0000

    Better debug time overrides

--- alarmclock.py
+++ alarmclock.py
@@ -30,24 +30,37 @@ BUTTON_DISPLAY_DURATION = 5.0
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--hour", type=int,
-                    help="hour override (for testing)")
+                    help="fixed hour override (for testing)")
 parser.add_argument("--min", type=int,
-                    help="minute override (for testing)")
+                    help="fixed minute override (for testing)")
 args = parser.parse_args()
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 alarm_status = AlarmStatus.WAITING
 alarm_sound = sa.WaveObject.from_wave_file(os.path.join(__location__, "tomorrow_starts_today.wav"))
 
-last_button_press = time.mktime(time.localtime()) # default to process start, so it displays on startup
-
 sn3218.disable()
+
+def get_localtime():
+    global args
+    now = time.localtime()
+    return time.struct_time((
+        now.tm_year, 
+        now.tm_mon, 
+        now.tm_mday, 
+        coalesce(args.hour, now.tm_hour), 
+        coalesce(args.min, now.tm_min), 
+        now.tm_sec, 
+        now.tm_wday, 
+        now.tm_yday, 
+        now.tm_isdst
+    ))
 
 def button_callback(channel):
     global alarm_status
     global last_button_press
 
-    last_button_press = time.mktime(time.localtime())
+    last_button_press = time.mktime(get_localtime())
     print("Button pressed")
 
     if alarm_status==AlarmStatus.PLAYING:
@@ -60,35 +73,36 @@ try:
     GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(37, GPIO.RISING, callback=button_callback, bouncetime=300)
 
+    last_button_press = time.mktime(get_localtime()) # default to process start, so it displays on startup
+    alarm_play_obj = None
+
     while True:
         scrollphathd.clear()
 
-        current_time = time.localtime()
-        current_hour = coalesce(args.hour, current_time.tm_hour)
-        current_min = coalesce(args.min, current_time.tm_min)
-        current_sec = current_time.tm_sec
+        current_time = get_localtime()
 
-        if current_hour==ALARM_HOUR and current_min==ALARM_MIN:
+        if current_time.tm_hour==ALARM_HOUR and current_time.tm_min==ALARM_MIN:
             if alarm_status==AlarmStatus.WAITING:
                 alarm_status = AlarmStatus.PLAYING
                 alarm_play_obj = alarm_sound.play()
         else:
             alarm_status = AlarmStatus.WAITING
             
-        if alarm_status==AlarmStatus.PLAYING and current_sec % 2 == 0:
+        if alarm_status==AlarmStatus.PLAYING and current_time.tm_sec % 2 == 0:
             scrollphathd.fill(BRIGHTNESS, x=0, y=6, width=17, height=1)
             if not alarm_play_obj.is_playing():
                 alarm_play_obj = alarm_sound.play()
 
-        if alarm_status==AlarmStatus.STOPPED and alarm_play_obj:
+        if (alarm_status==AlarmStatus.STOPPED or alarm_status==AlarmStatus.WAITING) and alarm_play_obj:
             alarm_play_obj.stop()
         
-        if ((current_hour >= DISPLAY_ON_HOUR and current_hour < DISPLAY_OFF_HOUR) or
+        if ((current_time.tm_hour >= DISPLAY_ON_HOUR and current_time.tm_hour < DISPLAY_OFF_HOUR) or
             alarm_status==AlarmStatus.PLAYING or
             time.mktime(current_time)-last_button_press <= BUTTON_DISPLAY_DURATION):
-            display_hour = ((current_hour-1) % 12) + 1
+            # Display the time
+            display_hour = ((current_time.tm_hour-1) % 12) + 1
             scrollphathd.write_string(
-                "{:>2}:{:0>2}".format(display_hour, current_min),
+                "{:>2}:{:0>2}".format(display_hour, current_time.tm_min),
                 x=0, y=0, font=font3x5, brightness=BRIGHTNESS
             )
 
